e30b8d6595f9e04128b95bce26f5299f3e45fa98
nazarPtyt <ockap90@libero.it>
2022-03-31 15:01:52 +0200

    3-th task is done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
+    addUser: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    error: string
+    totalUsers: number
+    onEnter: (e: KeyboardEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
-) => {
-    const inputClass = s.error // need to fix with (?:)
+    {name, setNameCallback, addUser, error, onEnter, totalUsers}) => {  // деструктуризация пропсов
+
+    const inputClass = !error ? (s.approved) : s.errorBorder;
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
+        <div className={s.taskWrap}>
+            <div className={s.inputWrap}>
+                <input
+                    value={name}
+                    onChange={setNameCallback}
+                    onKeyPress={onEnter}
+                    className={name.length > 0 ? s.approved : ''}
+                />
+                <span className={s.error}>{error}</span>
+            </div>
+            <button onClick={addUser} disabled={!name}>add</button>
             <span>{totalUsers}</span>
         </div>
     )
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,29 +1,43 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[]
+    addUserCallback: (name: string) => void
 }
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => {  //деструктуризация пропсов
+    const [name, setName] = useState<string>('')
+    const [error, setError] = useState<string>('')
 
-// более современный и удобный для про :)
-// уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        const trimName = e.currentTarget.value.trim()
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+        if (trimName) {
+            setName(trimName)
+            setError('')
+        } else {
+            setError('error')
+            setName('')
+        }
+
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        addUserCallback(name)
+        alert(`Hello ${name}!`)
+        setName('')
+
     }
+    const totalUsers = users.length
 
-    const totalUsers = 0 // need to fix
-
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && name) {
+            addUser()
+        } else {
+            setError('Field is required !!!')
+        }
+    }
     return (
         <Greeting
             name={name}
@@ -31,6 +45,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+            onEnter={onEnter}
         />
     )
 }
