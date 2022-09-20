3c49250dd0d872a573cc8400849adf96baf27af9
nazarPtyt <ockap90@libero.it>
2022-09-20 13:07:41 +0200

    Updates

---
+++ precache-manifest.84868911004d9d9f8496cbdc36f4b9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e278a2bd16607c5f82af9c54e558f5",
+    "url": "/Tasks-from-Ignat/index.html"
+  },
+  {
+    "revision": "5f478040dc7ad6f0cb83",
+    "url": "/Tasks-from-Ignat/static/css/main.bdc29f0c.chunk.css"
+  },
+  {
+    "revision": "803d6477b29a556328b5",
+    "url": "/Tasks-from-Ignat/static/js/2.17f38821.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Tasks-from-Ignat/static/js/2.17f38821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f478040dc7ad6f0cb83",
+    "url": "/Tasks-from-Ignat/static/js/main.46eebd8b.chunk.js"
+  },
+  {
+    "revision": "ceae675045bccf3ebab6",
+    "url": "/Tasks-from-Ignat/static/js/runtime-main.8fa9b17d.js"
+  }
+]);
