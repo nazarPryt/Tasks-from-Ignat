f38b0f73f6560e03ed2b1ea058187f71db41aff0
nazarPtyt <ockap90@libero.it>
2022-09-22 12:01:31 +0200

    5-th task seems to be done

--- src/p2-homeworks/h5-rrd-v6/Burger.tsx
+++ src/p2-homeworks/h5-rrd-v6/Burger.tsx
@@ -1,43 +1,28 @@
 import React, {useState} from 'react';
-import s from'./Burger.module.css'
+import s from './Burger.module.css'
 import {Link} from "react-router-dom";
 import {PATH} from "./Pages";
+import logo from './cheeseburger.png'
+
 const Burger = () => {
 
-    // to change burger classes
-    const [burger_class, setBurgerClass] = useState("burgerBar unclicked")
-    const [menu_class, setMenuClass] = useState("menu hidden")
-    const [isMenuClicked, setIsMenuClicked] = useState(false)
+const [click , setClick] = useState<boolean>(true)
+    console.log(click)
 
-    // toggle burger menu change
-    const updateMenu = () => {
-        if (!isMenuClicked) {
-            setBurgerClass("burgerBar clicked")
-            setMenuClass("menu visible")
-        } else {
-            setBurgerClass("burgerBar unclicked")
-            setMenuClass("menu hidden")
-        }
-        setIsMenuClicked(!isMenuClicked)
+
+
+    const onClickHandler = () => {
+        setClick(!click)
     }
-
     return (
-        <div>
-            <nav className={s.nav}>
-                <div className={s.burgerMenu} onClick={updateMenu}>
-                    <div className={burger_class}></div>
-                    <div className={burger_class}></div>
-                    <div className={burger_class}></div>
-                </div>
-
-            </nav>
-
-            <div className={`${menu_class}`}>
-                <div className={s.navLink_Wrap}>
+        <div className={click ? `${s.burgerWrap_open}` : `${s.burgerWrap_close}`}>
+            <nav className={s.navLink_Wrap}>
                 <Link className={s.navLink} to={PATH.PRE_JUNIOR}>Pre-Junior</Link>
                 <Link className={s.navLink} to={PATH.JUNIOR}>Junior</Link>
                 <Link className={s.navLink} to={PATH.JUNIOR_PLUS}>Junior-PLUS</Link>
-                </div>
+            </nav>
+            <div className={s.burgerIcon} onClick={onClickHandler}>
+                <img src={logo} alt=""/>
             </div>
         </div>
     )
--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import {Link} from "react-router-dom";
-import {PATH} from "./Pages";
-import s from './HW5.module.css'
 import Burger from "./Burger";
 
 
 function Header() {
     return (
         <div>
-            {/*<nav className={s.navWrap}>*/}
-            {/*    <Link className={s.navLink} to={PATH.PRE_JUNIOR}>Pre-Junior</Link>*/}
-            {/*    <Link className={s.navLink} to={PATH.JUNIOR}>Junior</Link>*/}
-            {/*    <Link className={s.navLink} to={PATH.JUNIOR_PLUS}>Junior-PLUS</Link>*/}
-            {/*</nav>*/}
             <Burger/>
         </div>
     )
