213aab88fd1d3ee8a7b3bbdea0e58358a66b7aab
nazarPtyt <ockap90@libero.it>
2022-09-28 22:21:23 +0200

    9-th Task is done

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import s from './App.module.css'
-import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5";
+import HW9 from "../../../p2-homeworks/h9/HW9";
 
 function App() {
     return (
 
         <div className={s.App}>
-            <HW5/>
+            {/*<HW5/>*/}
+            <HW9/>
         </div>
 
     )
