f9967bf8e30433cf792606bc0096629453cf27de
nazarPtyt <ockap90@libero.it>
2022-10-11 22:54:38 +0200

    10-th Task adjusted Store component + added React-redux

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import s from './App.module.css'
-import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5";
+import HW10 from "../../../p2-homeworks/h10/HW10";
+import {Provider} from "react-redux";
+import store from "../../../p2-homeworks/h10/bll/store";
 
 function App() {
     return (
+        <Provider store={store}>
+            <div className={s.App}>
+                {/*<HW5/>*/}
+                <HW10/>
+            </div>
+        </Provider>
 
-        <div className={s.App}>
-            <HW5/>
-        </div>
 
     )
 }
